ff5e662aab2d8c26fc5db3875a0fdff364e54001
Bharath <gl.bharath2006@gmail.com>
2026-01-31 14:36:48 +0530

    Add Solution Method

--- Routes/questionRoutes.js
+++ Routes/questionRoutes.js
@@ -8,6 +8,7 @@ const questionRoutes = express.Router();
 questionRoutes.route("/").get(questionController.getAllQuestions).post(userController.protect,questionController.createQuestion);
 questionRoutes.route("/my-questions").get(userController.protect, questionController.getMyQuestions);
 questionRoutes.route("/:id").get(questionController.getQuestion).delete(questionController.deleteQuestion);
+questionRoutes.route("/add-solution/:id").post(userController.protect,questionController.addSolution);
 
 
 module.exports = questionRoutes;
